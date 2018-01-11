963658bd67d987d070d32e40f91740438b28fc9f
Chris Hanes <chris.hanes@digital.justice.gov.uk>
2018-01-11 12:29:12 +0000

    Add gender gap and update readme

--- README.md
+++ README.md
@@ -4,6 +4,8 @@ These statsistics show the measurements of the Justice Digital & Technology team
 
 Statistics are gathered from a Google sheet (https://docs.google.com/spreadsheets/d/1B37J7fFTokDZ3J3hX4zIm4YhGy5tR6WfKt6RRIfugcE).
 
+This repo publishes to the Git Page https://ministryofjustice.github.io/normalising-stats
+
 ## Get started
 
 Run:
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -14,19 +14,27 @@
         google.charts.load('current', {
           'packages': ['corechart']
         });
-        google.charts.setOnLoadCallback($.proxy(this.drawCharts, this));
+
+        google.charts.setOnLoadCallback($.proxy(this.getChartData, this));
       }
     },
 
-    drawCharts: function() {
+    getChartData: function() {
       var intro = new google.visualization.Query('https://docs.google.com/spreadsheets/d/1B37J7fFTokDZ3J3hX4zIm4YhGy5tR6WfKt6RRIfugcE/gviz/tq?gid=0&range=B2');
       var footer = new google.visualization.Query('https://docs.google.com/spreadsheets/d/1B37J7fFTokDZ3J3hX4zIm4YhGy5tR6WfKt6RRIfugcE/gviz/tq?gid=0&range=B6');
       var query = new google.visualization.Query('https://docs.google.com/spreadsheets/d/1B37J7fFTokDZ3J3hX4zIm4YhGy5tR6WfKt6RRIfugcE/gviz/tq?gid=0&range=B9:F14');
       var age = new google.visualization.Query('https://docs.google.com/spreadsheets/d/1B37J7fFTokDZ3J3hX4zIm4YhGy5tR6WfKt6RRIfugcE/gviz/tq?gid=0&range=B15:F25');
+
       intro.send($.proxy(this.handleIntroResponse, this));
       footer.send($.proxy(this.handleFooterResponse, this));
       query.send($.proxy(this.handleChartResponse, this));
       age.send($.proxy(this.handleAgeResponse, this));
+
+    },
+
+    getPayData: function(){
+      var query = new google.visualization.Query('https://docs.google.com/spreadsheets/d/1B37J7fFTokDZ3J3hX4zIm4YhGy5tR6WfKt6RRIfugcE/gviz/tq?gid=0&range=B35:E38');
+      query.send($.proxy(this.handlePayResponse, this));
     },
 
     handleIntroResponse: function(response){
@@ -51,6 +59,25 @@
                 .append('<p>'+resp.getValue(0, 1)+'</p>')
                 .insertAfter(this.$chartEl);
       this.addAges(resp, $el);
+    },
+
+    handlePayResponse: function(response) {
+      var resp = response.getDataTable();
+      var $el = $('<div/>')
+                .addClass('column-full')
+                .append('<h2 class="heading-small">'+resp.getValue(0, 0)+'</h2>')
+                .append('<p>'+resp.getValue(1, 0)+'</p>')
+                .append('<p>'+resp.getValue(2, 0)+'</p>')
+                .append('<p>'+resp.getValue(3, 0)+'</p>')
+                .appendTo($('#women'));
+      var $mean = $('<div/>').addClass('data column-one-third')
+                  .append('<span class="data-item bold-xxlarge">'+resp.getValue(1, 2)+'%</span>')
+                  .append('<span class="data-item bold-xsmall">Lower than men (MEAN)</span>')
+                  .appendTo($('#women'));
+      var $median = $('<div/>').addClass('data column-one-third')
+                  .append('<span class="data-item bold-xxlarge">'+resp.getValue(1, 3)+'%</span>')
+                  .append('<span class="data-item bold-xsmall">Lower than men (MEDIAN)</span>')
+                  .appendTo($('#women'));
     },
 
     addAges: function(resp, $el) {
@@ -86,17 +113,23 @@
 
     addRows: function(resp) {
       for(var i=0; i<resp.getNumberOfRows();i++){
-        var $el = $('<div/>')
-                    .append('<h2 class="heading-medium">'+resp.getValue(i, 0)+'</h2>')
-                    .append('<p>'+resp.getValue(i, 1)+'</p>')
+        var title = resp.getValue(i, 0),
+            $el = $('<div/>')
+                    .addClass('grid-row')
+                    .attr('id', title.replace(/\s/g,'').toLowerCase())
+                    .append('<h2 class="column-two-thirds heading-medium">'+resp.getValue(i, 0)+'</h2>')
+                    .append('<p class="column-full">'+resp.getValue(i, 1)+'</p>')
                     .appendTo(this.$chartEl);
         this.addChart(resp, i, $el);
       }
+
+      this.getPayData();
+
     },
 
     addChart: function(resp, row, $el){
       for(var i=3; i<resp.getNumberOfColumns();i++){
-        var $graphEl = $('<div/>').addClass('column').appendTo(this.$chartEl);
+        var $graphEl = $('<div/>').addClass('column-one-half column').appendTo($el);
 
         var data = new google.visualization.DataTable(),
           options = {
@@ -133,7 +166,7 @@
         chart.draw(data, options);
         var $valueEl = $('<div/>').addClass('value').text(Math.round(( resp.getValue(row,i) * 100).toFixed(1)) + '%').appendTo($graphEl);
       }
-    }
+    },
 
   };
 
